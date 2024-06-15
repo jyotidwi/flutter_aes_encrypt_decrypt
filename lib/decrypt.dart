4e2186e927022ec943411f01eb096ef1352b2cfc
ज्योति द्विवेदी <128375571+Jyotidwi@users.noreply.github.com>
2024-06-15 10:21:45 +0530

    Update decrypt.dart

--- lib/decrypt.dart
+++ lib/decrypt.dart
@@ -2,29 +2,29 @@ import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/foundation.dart';
 
 class MyEncryptionDecryption {
-  final String _aesKey="buyaohuida-buyaohuida-buyaohuida";
+  final String _aesKey = "buyaohuida-buyaohuida-buyaohuida-bu";
 
-  Future<String> decryptt({required String data}) async {
+  Future<String> decrypt({required String data}) async {
     final key = encrypt.Key.fromUtf8(_aesKey);
-    final iv = encrypt.IV.fromLength(16);
+    final iv = encrypt.IV.fromBase64(data.substring(0, 24));
     final encrypter = encrypt.Encrypter(
         encrypt.AES(key, mode: encrypt.AESMode.sic, padding: 'PKCS7'));
-    final decrypted = encrypter.decrypt64(data, iv: iv);
+    final decrypted = encrypter.decrypt64(data.substring(24), iv: iv);
     if (kDebugMode) {
       print(decrypted);
     }
     return decrypted;
   }
-  Future<String> encryptt({required String data}) async {
-    final key = encrypt.Key.fromUtf8(_aesKey);
-    final iv = encrypt.IV.fromLength(16);
-    final encrypter = encrypt.Encrypter(
-        encrypt.AES(key, mode: encrypt.AESMode.cbc, padding: 'PKCS7'));
-    final encryptted = encrypter.encrypt(data, iv: iv);
-    if (kDebugMode) {
-      print(encryptted);
-    }
-    return encryptted.base64;
-  }
 
+  Future<String> encrypt({required String data}) async {
+    final key = encrypt.Key.fromUtf8(_aesKey);
+    final iv = encrypt.IV.fromSecureRandom(16);
+    final encrypter = encrypt.Encrypter(
+        encrypt.AES(key, mode: encrypt.AESMode.sic, padding: 'PKCS7'));
+    final encrypted = encrypter.encrypt(data, iv: iv);
+    if (kDebugMode) {
+      print(encrypted);
+    }
+    return iv.base64 + encrypted.base64;
+  }
 }
