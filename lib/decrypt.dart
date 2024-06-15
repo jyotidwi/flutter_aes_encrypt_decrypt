acc4294929aba0bf3b2aaccbc05ba5f26cd280db
ज्योति द्विवेदी <128375571+Jyotidwi@users.noreply.github.com>
2024-06-15 19:21:59 +0530

    Update decrypt.dart

--- lib/decrypt.dart
+++ lib/decrypt.dart
@@ -3,10 +3,15 @@ import 'package:flutter/foundation.dart';
 
 class MyEncryptionDecryption {
   final String _aesKey = "buyaohuida-buyaohuida-buyaohuida";
+  late encrypt.IV _ivUsedForEncryption;
+
+  String get aesKey => _aesKey;
+  encrypt.IV get ivUsedForEncryption => _ivUsedForEncryption;
 
   Future<String> decryptt({required String data}) async {
     final key = encrypt.Key.fromUtf8(_aesKey);
     final iv = encrypt.IV.fromBase64(data.substring(0, 24));
+    _ivUsedForEncryption = iv; // Save IV for display
     final encrypter = encrypt.Encrypter(
         encrypt.AES(key, mode: encrypt.AESMode.sic, padding: 'PKCS7'));
     final decrypted = encrypter.decrypt64(data.substring(24), iv: iv);
@@ -19,6 +24,7 @@ class MyEncryptionDecryption {
   Future<String> encryptt({required String data}) async {
     final key = encrypt.Key.fromUtf8(_aesKey);
     final iv = encrypt.IV.fromSecureRandom(16);
+    _ivUsedForEncryption = iv; // Save IV for display
     final encrypter = encrypt.Encrypter(
         encrypt.AES(key, mode: encrypt.AESMode.sic, padding: 'PKCS7'));
     final encrypted = encrypter.encrypt(data, iv: iv);
